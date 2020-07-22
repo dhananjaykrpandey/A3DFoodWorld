e3c28df51bd5c0bb0f9f8403ea1a044c49c78d5f
Dhananjay K. Pandey (NPCC) <dhananjayp@npcc.ae>
2020-07-22 13:27:57 +0400

    Add project files.

---
+++ A3DFoodWorld/A3DFoodWorld/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using A3DFoodWorld.Models;
+
+namespace A3DFoodWorld.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
